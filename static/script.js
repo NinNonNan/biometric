a23a5f81d419fd43ca8de29c1e639d3134b07cd6
nino <nino@isideweb.com>
2026-01-13 10:10:37 +0100

    Struttura rivista ed effetto crt aggiunto

---
+++ static/script.js
@@ -0,0 +1,21 @@
+function fetchData() {
+    fetch('/data')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('hr').innerText = data.hr + ' BPM';
+
+            const stateEl = document.getElementById('state');                    
+            stateEl.innerText = data.state;
+            if (data.state === 'CRITICAL') {
+                stateEl.classList.add('critical');
+            } else {
+                stateEl.classList.remove('critical');
+            }
+            
+            document.getElementById('avg').innerText = data.hr_avg;
+            document.getElementById('delta').innerText = data.delta_hr;
+            document.getElementById('stress').innerText = data.stress;
+            document.getElementById('ts').innerText = data.ts;
+        });
+}
+setInterval(fetchData, 1000);
